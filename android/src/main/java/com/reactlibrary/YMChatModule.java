4c1eb17454ec18840ae128d2649698a63349ea65
Purush <purushottam.yadav@yellowmessenger.com>
2021-02-24 19:12:40 +0530

    Created android skeleton

--- android/src/main/java/com/reactlibrary/YMChatModule.java
+++ android/src/main/java/com/reactlibrary/YMChatModule.java
@@ -9,10 +9,23 @@ import com.facebook.react.bridge.Callback;
 public class YMChatModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  private final YMChatService ymChatService= new YMChatService();
 
   public YMChatModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+  }
+
+  @ReactMethod
+  void initialize(String botId)
+  {
+      ymChatService.setBotId(botId);
+  }
+  
+  @ReactMethod
+  void startChatBot()
+  {
+    ymChatService.startChatbot(reactContext);
   }
 
   @Override
--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import { NativeModules } from 'react-native';
 
 const { YMChat } = NativeModules;
 
+// function initialize(botid) {
+//     YMChat.initialize(botid);
+// }
+
+// function startChatbot() {
+//     YMChat.startChatbot();
+// }
+
+
 export default YMChat;
