aec141873e394fcfdb9de911fa3420ff0d108e97
Purush <purushottam.yadav@yellowmessenger.com>
2021-02-25 17:55:03 +0530

    Added event Listener and Payload

--- android/src/main/java/com/reactlibrary/YMChatModule.java
+++ android/src/main/java/com/reactlibrary/YMChatModule.java
@@ -10,28 +10,37 @@ import com.facebook.react.bridge.Callback;
 
 public class YMChatModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
-  private final YMChatService ymChatService= new YMChatService();
+    private final ReactApplicationContext reactContext;
+    private final YMChatService ymChatService = new YMChatService();
 
-  public YMChatModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public YMChatModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @ReactMethod
-  public void initialize(String botId) {
-    Log.d("YMChat Android","Inititalizing chatbot.");
-      ymChatService.setBotId(botId);
-  }
-  
-  @ReactMethod
-  public void startChatBot()
-  {
-    ymChatService.startChatbot(reactContext);
-  }
+    @ReactMethod
+    public void initialize(String botId) {
+        Log.d("YMChat Android", "Inititalizing chatbot.");
+        ymChatService.setBotId(botId);
+    }
 
-  @Override
-  public String getName() {
-    return "YMChat";
-  }
+    @ReactMethod
+    public void startChatBot() {
+        ymChatService.startChatbot(reactContext);
+    }
+
+    @ReactMethod
+    public void eventListener(Callback callbackListener) {
+    ymChatService.eventListener(callbackListener);
+    }
+
+    @ReactMethod
+    public void addPayload(String key, String value) {
+        ymChatService.addPayload(key, value);
+    }
+
+    @Override
+    public String getName() {
+        return "YMChat";
+    }
 }
--- android/src/main/java/com/reactlibrary/YMChatService.java
+++ android/src/main/java/com/reactlibrary/YMChatService.java
@@ -1,36 +1,44 @@
 package com.reactlibrary;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.facebook.react.bridge.Callback;
 import com.yellowmessenger.ymchat.BotEventListener;
 import com.yellowmessenger.ymchat.YMChat;
 import com.yellowmessenger.ymchat.YMConfig;
 import com.yellowmessenger.ymchat.models.YMBotEventResponse;
 
+import java.security.Key;
 import java.util.HashMap;
 
 public class YMChatService {
     YMChat ymChat;
 
-    YMChatService()
-    {
-       this.ymChat =  YMChat.getInstance();
+    HashMap<String, Object> payloadData = new HashMap<>();
+
+    YMChatService() {
+        this.ymChat = YMChat.getInstance();
     }
 
-    public void setBotId(String botId){
+    public void setBotId(String botId) {
         ymChat.config = new YMConfig(botId);
-        HashMap<String, Object> payloadData = new HashMap<>();
+        ymChat.config.ymAuthenticationToken = "";
         ymChat.config.payload = payloadData;
     }
 
-    public void startChatbot(Context context)
-    {
+    public void startChatbot(Context context) {
         ymChat.startChatbot(context);
     }
 
-    public void EventListener(BotEventListener eventListener)
-    {
-        ymChat.onEventFromBot(eventListener);
+    public void eventListener(Callback callbackListener) {
+        ymChat.onEventFromBot(botEvent -> {
+            callbackListener.invoke(botEvent.getCode(),botEvent.getData());
+        });
+    }
+
+    public void addPayload(String key, String value) {
+        ymChat.config.payload.put(key, value);
     }
 }
 
