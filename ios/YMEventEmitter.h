024f1e2cd28c80de6d64bb408dfd86b55306eb3b
Kaunteya <k.suryawanshi@gmail.com>
2021-03-16 16:01:31 +0530

    Removed the headers

--- ios/YMEventEmitter.h
+++ ios/YMEventEmitter.h
@@ -1,11 +1,3 @@
-//
-//  Emitter.h
-//  YMChatReactNative
-//
-//  Created by Kauntey Suryawanshi on 15/03/21.
-//  Copyright © 2021 Facebook. All rights reserved.
-//
-
 #import "RCTEventEmitter.h"
 #import "RCTBridgeModule.h"
 
