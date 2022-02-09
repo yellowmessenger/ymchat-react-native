334bf483da85ad90c50f379932ea4bd8dd1f724f
Purush <75371296+PurushYM@users.noreply.github.com>
2022-02-09 17:06:20 +0530

    code refactor

--- ios/YMChatReactNative.m
+++ ios/YMChatReactNative.m
@@ -110,15 +110,15 @@ RCT_EXPORT_METHOD(setCloseButtonColour:(NSString *) colour) {
 }
 
 - (UIColor *)getColorFromHexString:(NSString *)hexString {
-    if ([(NSString*) hexString characterAtIndex:0] == '#')
-    {
-        unsigned rgbValue = 0;
-        NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    if ([(NSString*) hexString characterAtIndex:0] == '#') {
         [scanner setScanLocation:1]; // bypass '#' character
-        [scanner scanHexInt:&rgbValue];
-        return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+    } else {
+        [scanner setScanLocation:0];
     }
-    return  nil;
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 
